c31ac7573a93ff6458ccf4d8562a9e774295d85b
TiagoTiciano <tiagobolinha@gmail.com>
2018-10-11 23:25:00 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="pt-BR" />
+<meta http-equiv="Cache-Control" content="no-cache" />
+
+<meta name="robots" content="ALL" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="resource-types" content="document" />
+<meta  http-equiv="pragma" content="no-cache" />
+<meta name="revisit-after" content="1 day" />
+<meta name="classification" content="Security" />
+<meta name="distribution" content="Global" />
+<meta name="rating" content="General" />
+<meta name="language" content="pt-br" />
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
+
+<title></title>
+</head>
+
+<frameset rows="50,*" frameborder="no" border="0" framespacing="0">  <frame src="/player" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="playertop" />  <frame src="/principal" name="mainFrame" id="mainFrame" title="mainFrame" /></frameset><noframes><body></body></noframes></html>
